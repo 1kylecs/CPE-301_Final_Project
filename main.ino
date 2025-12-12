5f5b5ec97852d8eb3408d6ec68397a38b24f2611
Kyle Kaiser <39283939+1kylecs@users.noreply.github.com>
2025-12-11 23:16:38 -0800

    Implement ADC initialization and read functions
    Added ADC initialization and reading functions using direct register access.

--- main.ino
+++ main.ino
@@ -1,6 +1,18 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
+/* HARDWARE REGISTERS */
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+ 
+volatile unsigned char *my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char *my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char *my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int *my_ADC_DATA = (unsigned int*) 0x78;
+
 /* CONSTANTS */
 const int trigPin = 3;
 const int echoPin = 2;
@@ -185,4 +197,59 @@ void myDigitalWrite(const int pin, const bool high) {
 }
 
 /* ANALOG READ */
+void adc_init()
+{
+  // setup the A register
+ // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA |= 0x80;
 
+ // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= ~0x40;
+
+ // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= ~0x08;
+
+ // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  *my_ADCSRA &= ~0x07;
+
+  // setup the B register
+// clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= ~0x08;
+
+ // clear bit 2-0 to 0 to set free running mode
+  *my_ADCSRB &= ~0x07;
+
+  // setup the MUX Register
+ // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX &= ~0x80;
+
+// set bit 6 to 1 for AVCC analog reference
+  *my_ADMUX |= 0x40;
+
+  // clear bit 5 to 0 for right adjust result
+  *my_ADMUX &= ~0x20;
+
+ // clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADMUX &= ~0x1F;
+}
+unsigned int adc_read(unsigned char adc_channel_num) //work with channel 0
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX &= ~0x1F;
+
+  // clear the channel selection bits (MUX 5) hint: it's not in the ADMUX register
+  *my_ADCSRB &= ~0x08;
+ 
+  // set the channel selection bits for channel 0
+  *my_ADMUX |= adc_channel_num;
+
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+
+  // return the result in the ADC data register and format the data based on right justification (check the lecture slide)
+  unsigned int val = *my_ADC_DATA;
+  return val;
+}
